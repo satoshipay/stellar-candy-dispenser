be7c67b32faf1721f95770d91dc38273a48b53e1
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-11-21 10:22:14 +0100

    Change transaction handling + add polling - Check if transactions are valid (contain a payment operation with a minimum amount) - Do an initial fetch to receive the latest transaction

--- src/index.js
+++ src/index.js
@@ -4,44 +4,125 @@ const servo = require("./servo");
 
 require("dotenv").config();
 
-const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
 const raspberryAccountID = process.env.ACCOUNT_PUBLIC_KEY;
+
+const horizonURL =
+  process.env.NODE_ENV === "production"
+    ? "https://horizon.stellar.org"
+    : "https://horizon-testnet.stellar.org";
+const server = new StellarSdk.Server(horizonURL);
 
 const promiseQueue = new PQueue({ concurrency: 1 });
 
 let latestCursor = "0";
 let latestCreatedAt = "";
 
-const motor1 = servo(18);
-const motor2 = servo(23);
+const motorRight = servo(18);
+const motorLeft = servo(23);
 
-async function listenForTransactions(accountID) {
+async function listenForTransactions() {
   server
     .transactions()
-    .forAccount(accountID)
+    .forAccount(raspberryAccountID)
     .cursor("now")
     .stream({
       onmessage: function(transaction) {
-        latestCursor = transaction.paging_token;
-        latestCreatedAt = transaction.created_at;
-        promiseQueue.add(() => handleTransaction(transaction));
+        if (Date.parse(latestCreatedAt) < Date.parse(transaction.created_at)) {
+          latestCursor = transaction.paging_token;
+          latestCreatedAt = transaction.created_at;
+          promiseQueue.add(() => handleTransaction(transaction));
+        } else {
+          console.log("Discarding transaction, already handled");
+        }
       }
     });
 }
 
 async function handleTransaction(transaction) {
-  // TODO check memos and values (e.g. 1XLM received and memo contains info)
-
   console.log("Handling transaction", transaction.id);
-  await motor1.executeTurn();
+
+  try {
+    let containsValidPaymentOperation = false;
+    const operations = await transaction.operations();
+    for (const operation of operations.records) {
+      if (
+        operation.to === raspberryAccountID &&
+        operation.type === "payment" &&
+        Number(operation.amount) >= 1
+      ) {
+        containsValidPaymentOperation = true;
+      }
+    }
+
+    if (!containsValidPaymentOperation) {
+      console.log(
+        `Transaction ${transaction.id} does not contain a valid payment operation`
+      );
+      return;
+    }
+
+    if (transaction.memo && transaction.memo.toLowerCase().startsWith("left")) {
+      await motorLeft.executeTurn();
+    } else {
+      await motorRight.executeTurn();
+    }
+  } catch (error) {
+    console.error(
+      `Something went wrong while handling transaction ${transaction.id}:`,
+      error
+    );
+  }
 }
 
 async function startPolling() {
-  // TODO poll for new transactions
+  const { records } = await server
+    .transactions()
+    .forAccount(raspberryAccountID)
+    .cursor(latestCursor)
+    .order("asc")
+    .call();
+
+  console.log("Polled amount of transactions", records.length);
+
+  records.forEach(transaction => {
+    if (Date.parse(latestCreatedAt) < Date.parse(transaction.created_at)) {
+      latestCursor = transaction.paging_token;
+      latestCreatedAt = transaction.created_at;
+      promiseQueue.add(() => handleTransaction(transaction));
+    } else {
+      console.log("Discarding transaction, already handled");
+    }
+  });
+
+  if (records.length > 0) {
+    const latestTransaction = records[0];
+    latestCursor = latestTransaction.paging_token;
+    latestCreatedAt = latestTransaction.created_at;
+  }
 }
 
-function init() {
+async function initialFetch() {
+  // fetch latest transaction to get cursor + created_at
+  const { records } = await server
+    .transactions()
+    .forAccount(raspberryAccountID)
+    .limit(1)
+    .order("desc")
+    .call();
+
+  if (records.length > 0) {
+    const latestTransaction = records[0];
+    latestCursor = latestTransaction.paging_token;
+    latestCreatedAt = latestTransaction.created_at;
+
+    console.log("Set latestTransaction info to", latestCursor, latestCreatedAt);
+  }
+}
+
+async function init() {
   listenForTransactions(raspberryAccountID);
+
+  await initialFetch();
 
   setInterval(() => {
     startPolling();
